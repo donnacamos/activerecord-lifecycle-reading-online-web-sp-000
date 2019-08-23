325bfa55d8abc8c63bf99e842455a4c7771184bf
Donna Amos <donnacamos.developer@gmail.com>
2019-08-23 21:03:09 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :author
-  resources :post
+  resources :post, only [:edit, :index, :new, :show] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
